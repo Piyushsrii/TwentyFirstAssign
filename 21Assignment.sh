ec5f0fe3709cf6d291bc0c9be4b936a8da355b99
piyushsrii <piyushnamo888@gmail.com>
2020-08-02 15:41:09 +0530

    commit in uc1

--- 21Assignment.sh
+++ 21Assignment.sh
@@ -1,3 +1,24 @@
 Welcome in Assignment 21
 
+Problem statement----->(Check Employee is Present or Absent- Use ((RANDOM)) for Attendance Check)
+public class Employee1
+{
+          public void checkEmployee()
+          {
+                  double empcheck=Math.floor(Math.random()*10)%2;
+                  if(empcheck==1)
+                  {
+                  System.out.println("Employee is present");
+                  }
+                  else
+                  {
+                  System.out.println("Employee is not present");
+                  }
+          }
+    public static void main(String[] args)
+    {
+        Employee1 emp=new Employee();
+        emp.checkEmployee();
+    }
+}
 
